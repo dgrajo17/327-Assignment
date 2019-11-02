6e6916ba82122f1f206782741486d98a236a0cd3
Hojun Lee <15hl40@caslab.queensu.ca>
2019-11-02 16:20:40 -0400

    added test code

--- p3/test_pythonTestCode.py
+++ p3/test_pythonTestCode.py
@@ -619,49 +619,49 @@ def test_r15a(capsys):
 #         capsys=capsys,
 #         test_id='23b'
 #     )
-def test_r24a(capsys):
-    """Testing r2. All required information stored in folder r2. 
+# def test_r24a(capsys):
+    # """Testing r2. All required information stored in folder r2. 
 
-    Arguments:
-        capsys -- object created by pytest to capture stdout and stderr
-    """
-    helperNoTSF(
-        capsys=capsys,
-        test_id='24a'
-    )
+    # Arguments:
+        # capsys -- object created by pytest to capture stdout and stderr
+    # """
+    # helperNoTSF(
+        # capsys=capsys,
+        # test_id='24a'
+    # )
 
-def test_r24b(capsys):
-    """Testing r2. All required information stored in folder r2.
+# def test_r24b(capsys):
+    # """Testing r2. All required information stored in folder r2.
 
-    Arguments:
-        capsys -- object created by pytest to capture stdout and stderr
-    """
-    helperNoTSF(
-        capsys=capsys,
-        test_id='24b'
-    )
+    # Arguments:
+        # capsys -- object created by pytest to capture stdout and stderr
+    # """
+    # helperNoTSF(
+        # capsys=capsys,
+        # test_id='24b'
+    # )
 
-def test_r25a(capsys):
-    """Testing r2. All required information stored in folder r2.
+# def test_r25a(capsys):
+    # """Testing r2. All required information stored in folder r2.
 
-    Arguments:
-        capsys -- object created by pytest to capture stdout and stderr
-    """
-    helperNoTSF(
-        capsys=capsys,
-        test_id='25a'
-    )
+    # Arguments:
+        # capsys -- object created by pytest to capture stdout and stderr
+    # """
+    # helperNoTSF(
+        # capsys=capsys,
+        # test_id='25a'
+    # )
 	
-def test_r25b(capsys):
-    """Testing r2. All required information stored in folder r2.
+# def test_r25b(capsys):
+    # """Testing r2. All required information stored in folder r2.
 
-    Arguments:
-        capsys -- object created by pytest to capture stdout and stderr
-    """
-    helperNoTSF(
-        capsys=capsys,
-        test_id='25b'
-    )
+    # Arguments:
+        # capsys -- object created by pytest to capture stdout and stderr
+    # """
+    # helperNoTSF(
+        # capsys=capsys,
+        # test_id='25b'
+    # )
 
 def test_r26a(capsys):
     """Testing r2. All required information stored in folder r2.
