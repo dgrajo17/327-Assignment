ccc84241f9bb10581f6126bea217feda73222824
Dennis Grajo <15dlg3@queensu.ca>
2019-11-07 23:56:06 -0500

    Add alternate source code using hash tables

--- p4/backendSourceCode.py
+++ p4/backendSourceCode.py
@@ -2,53 +2,62 @@
 # REMINDER - AMOUNTS AND THEIR LIMITS ARE IN DECIMALS - BE SURE TO ADD TWO EXTRA ZEROS
 # WE USE A DICTIONARY (HASHMAP IN PYTHON) TO STORE DAILY TRANSAC LIMITS
 
+# the slides say "A newly created account must have a new unused account number"
+# Does that mean not being currently used or never used at all?
+# If never used at all, we have to use a hashmap to store all account numbers that were used
+# We currently use a hashmap following the above assumption
+# Store all accounts that were ever used, alongside their name
+accountsHash = {}
+
 
 def clearMasterTSF():
-	open('mergedTSF.txt', 'w').close()
-	
+    open('mergedTSF.txt', 'w').close()
+
 def mergeTSF():
-	for i in (tsfLIST):
-		tsf = open(str(tsfLIST+i),"r")
-		lines = tsf.readlines()
-		for line in lines:
-			newTSF = open(str('mergedTSF.txt'),"a")
-			if line != '':
-				if line[:3] != 'EOS':
-					newTSF.write(line)
-			newTSF.close()
-		tsf.close()
+    for i in (tsfLIST):
+        tsf = open(str(tsfLIST+i),"r")
+        lines = tsf.readlines()
+        for line in lines:
+            newTSF = open(str('mergedTSF.txt'),"a")
+            if line != '':
+                if line[:3] != 'EOS':
+                    newTSF.write(line)
+            newTSF.close()
+        newTSF = open(str('mergedTSF.txt'), "a")
+        newTSF.write("EOS 0000000 000 0000000 ***")
+        newTSF.close()
 
 def handleTSF():
-	tsf = open('mergedTSF.txt', 'r')
-	lines = tsf.readlines()
+    tsf = open('mergedTSF.txt', 'r')
+    lines = tsf.readlines()
     # Use dictionaries as hashmaps to store the transac limi
     dailyDepLimit = {}
     dailyWithLimit = {}
     dailyTransfLimit = {}
-	for line in lines:
+    for line in lines:
         tsfLine = line.split(" ", 5)
-		action = line[0]
+        action = line[0]
         accANum = line[1]
         amount = line[2]
         accBNum = line[3]
         accName = line[4]
-		#action = line[:3]
-		if action == 'DEP':
-			deposit(accANum, amount)
-		elif action == 'WDR':
-			withdraw(accANum, amount)
-		elif action == 'XFR':
-			transfer(accANum, amount, accBNum)
-		elif action == 'NEW':
-			create()
-		elif action == 'DEL':
-			delete()
+        #action = line[:3]
+        if action == 'DEP':
+            deposit(accANum, amount)
+        elif action == 'WDR':
+            withdraw(accANum, amount)
+        elif action == 'XFR':
+            transfer(accANum, amount, accBNum)
+        elif action == 'NEW':
+            create(accANum, accName)
+        elif action == 'DEL':
+            delete()
 
 def deposit(toAccNum, amount):
     if amount <= 200000:
         checkLimit = dailyDepLimit[toAccNum] + amount
         if checkLimit <= 500000:
-            maf = open('MAF.txt', 'w')
+            maf = open("MAF.txt", "w")
             lines = maf.readlines()
             for line in lines:
                 mafAccNum = line[:7]
@@ -71,7 +80,7 @@ def withdraw(fromAccNum, amount):
     if amount <= 100000:
         checkLimit = dailyWithLimit[fromAccNum] + amount
         if checkLimit <= 500000:
-            maf = open('MAF.txt', 'w')
+            maf = open("MAF.txt", "w")
             lines = maf.readlines()
             for line in lines:
                 mafAccNum = line[:7]
@@ -85,6 +94,7 @@ def withdraw(fromAccNum, amount):
                     else:
                         print("Error - insufficient funds")
                     break
+            maf.close()
         else:
             print("Exceeded daily withdraw limit")
     else:
@@ -96,7 +106,7 @@ def transfer(toAccNum, amount, fromAccNum):
         # Note: there is only a limit for transferring OUT of an account
         checkLimit = dailyTransfLimit[fromAccNum] + amount
         if checkLimit <= 1000000:
-            maf = open('MAF.txt', 'w')
+            maf = open("MAF.txt", "w")
             lines = maf.readlines()
             for line in lines:
                 mafAccNum = line[:7]
@@ -119,17 +129,34 @@ def transfer(toAccNum, amount, fromAccNum):
                     maf.write(mafAccNum + " " + mafAmountBalance + " " + mafAccName)
                     # I include the line below to remind y'all that we do NOT need a daily limit for the TO account
                     #dailyDepLimit[toAccNum] = dailyDepLimit[toAccNum] + amount
+            maf.close()
         else:
             print("Exceeded daily transfer limit")
     else:
         print("Exceeded single transfer limit")
 
+def updateAccountsHash()
+    maf = open("MAF.txt", "r")
+    lines = maf.readlines()
+    for line in lines:
+        mafLine = line.split(" ", 2)
+        mafAccNum = mafLine[0]
+        mafAccName = mafLine[2]
+        # Associate account name with account number
+        accountsHash[mafAccNum] = mafAccName
+
+# Use this function if deleted account numbers can be used for create
+def create(accNum, accName):
+    if 'accNum' in accountsHash:
+        maf = open("MAF.txt", "a")
+        maf.write(accNum + " " + "000" + " " + accName)
+        maf.close()
+
+#def createNewMAF():
+#    oldMaf = open('MAF.txt', 'w')
 
 
-
-def createMAF():
-
-	#maf = open('MAF.txt', 'w')
+    #maf = open('MAF.txt', 'w')
 
 
 #mainline
